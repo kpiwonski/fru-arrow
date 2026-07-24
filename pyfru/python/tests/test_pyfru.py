ee50fdab71f4236e4c40c82fd93a2129890baf83
Chris Piwonski <git@m.kpiwonski.com>
2026-07-25 01:44:25 +0200

    ruff

--- pyfru/python/tests/test_pyfru.py
+++ pyfru/python/tests/test_pyfru.py
@@ -292,11 +292,10 @@ def test_rf_cls_0_1_3ft_pickle_without_save_forest(table_0_1_3ft, tmp_path):
     rf.fit(X, y)
 
     model_path = tmp_path / "model.pkl"
-    with open(model_path, "wb") as f:
-        with pytest.raises(
-            ValueError, match="Cannot serialize model, when forest is not saved"
-        ):
-            pickle.dump(rf, f)
+    with open(model_path, "wb") as f, pytest.raises(
+        ValueError, match="Cannot serialize model, when forest is not saved"
+    ):
+        pickle.dump(rf, f)
 
 
 def test_rf_reg_importance(table_uniform_3ft):
